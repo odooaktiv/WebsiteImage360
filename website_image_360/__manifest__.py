2fa5b6d845f4cffc89a3a4f83df43483d1dfb6ea
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-06-30 17:57:21 +0530

    [ADD] Initial commit for WebsiteImage360 in branch master.

---
+++ website_image_360/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of AktivSoftware See LICENSE file for full copyright and licensing
+# details.
+{
+    'name': 'Website Image 360',
+    'summary': 'This module helps to rotate the product\
+    image in 360 dimension view',
+    'website': 'http://www.aktivsoftware.com',
+    'author': 'Aktiv Software',
+    'category': 'Website',
+    'version': '11.0.1.0.0',
+    'depends': ['website_sale'],
+    'data': [
+        'data/example_data_view.xml',
+        'views/website_image_360.xml',
+        'views/template.xml',
+    ],
+    'images': [
+        'static/description/banner.jpg',
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': False
+}
